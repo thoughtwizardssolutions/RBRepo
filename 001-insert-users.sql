73d69c96d3a69904324c30310debb4282188bee9
thoughtwizardssolutions <thoughtwizardssolutions@gmail.com>
2016-06-15 21:56:35 +0530

    New users

---
+++ 001-insert-users.sql
@@ -0,0 +1,9 @@
+insert into jhi_user values(1,'system','$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG','System', 'System', 'system@localhost',TRUE, 'en', null,null,'system', '2016-06-13 10:45:27.337',null,null,null);
+
+insert into jhi_user values(2,'anonymoususer','$2a$10$j8S5d7Sr7.8VTOYNviDPOeWX8KcYILUVJBsYV83Y5NtECayypx9lO','Anonymous', 'User', 'anonymous@localhost',TRUE, 'en', null,null,'system', '2016-06-13 10:45:27.337',null,null,null);
+
+
+insert into jhi_user values(3,'admin','$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC','Admin', 'Admin', 'admin@localhost',TRUE, 'en', null,null,'system', '2016-06-13 10:45:27.337',null,null,null);
+
+
+insert into jhi_user values(4,'user','$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K','User', 'User', 'system@localhost',TRUE, 'en', null,null,'system', '2016-06-13 10:45:27.337',null,null,null);
